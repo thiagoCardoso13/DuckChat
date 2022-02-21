fa079d885893eb06dca5569f62c3d773854109a1
thiagoCardoso13 <thiagobafc@gmail.com>
2022-02-21 22:40:27 +0000

    teste2

---
+++ app/src/main/java/thiagocardoso/pap/duckchat/activity/MainActivity.java
@@ -0,0 +1,98 @@
+package thiagocardoso.pap.duckchat.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import thiagocardoso.pap.duckchat.R;
+import thiagocardoso.pap.duckchat.config.ConfiguracaoFirebase;
+import thiagocardoso.pap.duckchat.fragment.ContatosFragment;
+import thiagocardoso.pap.duckchat.fragment.ConversasFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("DuckChat");
+        setSupportActionBar(toolbar);
+
+        //configurar as abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                .add("Conversas", ConversasFragment.class)
+                .add("Contatos", ContatosFragment.class)
+                .create()
+        );
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = findViewById(R.id.viewPagerTab);
+        viewPagerTab.setViewPager(viewPager);
+
+    }
+//chamando itens do menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+//deslogando usuário
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair:
+            deslogarUsuario();
+            finish();
+                break;
+            case R.id.menuConfiguracoes:
+                abrirConfiguracoes();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+
+        try{
+           autenticacao.signOut();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void abrirConfiguracoes(){
+        Intent intent = new Intent(MainActivity.this, ConfiguracoesActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/thiagocardoso/pap/duckchat/fragment/ContatosFragment.java
@@ -0,0 +1,146 @@
+package thiagocardoso.pap.duckchat.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import thiagocardoso.pap.duckchat.R;
+import thiagocardoso.pap.duckchat.adapter.ContatosAdapter;
+import thiagocardoso.pap.duckchat.config.ConfiguracaoFirebase;
+import thiagocardoso.pap.duckchat.model.Usuario;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ContatosFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContatosFragment extends Fragment {
+
+    private RecyclerView recyclerViewListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listaContatos = new ArrayList<>();
+    private DatabaseReference usuariosRef;
+    private ValueEventListener valueEventListenerContatos;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ContatosFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ContatosFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContatosFragment newInstance(String param1, String param2) {
+        ContatosFragment fragment = new ContatosFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        //Configurações iniciais
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+        usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+
+        //configurar adapter
+        adapter = new ContatosAdapter(listaContatos, getActivity());
+
+        //configurar recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewListaContatos.setLayoutManager(layoutManager);
+        recyclerViewListaContatos.setHasFixedSize(true);
+        recyclerViewListaContatos.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //recuperarcontatos
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        //usuariosRef.removeEventListener(valueEventListenerContatos);
+    }
+
+    public void recuperarContatos(){
+
+        valueEventListenerContatos = usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dados: snapshot.getChildren()){
+
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    listaContatos.add(usuario);
+
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+}
