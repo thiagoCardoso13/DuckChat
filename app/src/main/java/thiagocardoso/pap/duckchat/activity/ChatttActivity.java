c3640e2658b06c27d0fb4872e5389668f7437793
thiagoCardoso13 <thiagobafc@gmail.com>
2022-03-28 00:23:04 +0100

    Troca de mensagens funcionando e enviando imagens

--- app/src/main/java/thiagocardoso/pap/duckchat/activity/ChatttActivity.java
+++ app/src/main/java/thiagocardoso/pap/duckchat/activity/ChatttActivity.java
@@ -1,22 +1,33 @@
 package thiagocardoso.pap.duckchat.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,8 +40,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import thiagocardoso.pap.duckchat.R;
@@ -49,8 +62,11 @@ public class ChatttActivity extends AppCompatActivity {
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
     private EditText editMensagem;
+    private ImageView imageCamera;
+
     private Usuario usuarioDestinatario;
     private DatabaseReference database;
+    private StorageReference storage;
     private DatabaseReference mensagensRef;
     private ChildEventListener childEventListenerMensagens;
 
@@ -62,6 +78,7 @@ public class ChatttActivity extends AppCompatActivity {
     private MensagensAdapter adapter;
     private List<Mensagem> mensagens = new ArrayList<>();
 
+    private static final int SELECAO_GALERIA = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,6 +97,7 @@ public class ChatttActivity extends AppCompatActivity {
         circleImageViewFoto = findViewById(R.id.circleImageViewFotoChat);
         editMensagem = findViewById(R.id.editMensagem);
         recyclerMensagens = findViewById(R.id.recyclerMensagens);
+        imageCamera = findViewById(R.id.imageCamera);
 
         //recuperar dados do usuario remetente
         idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
@@ -115,12 +133,103 @@ public class ChatttActivity extends AppCompatActivity {
         recyclerMensagens.setHasFixedSize(true);
         recyclerMensagens.setAdapter(adapter);
 
+
+
         database = ConfiguracaoFirebase.getFirebaseDatabase();
+        storage = ConfiguracaoFirebase.getFirebaseStorage();
         mensagensRef = database.child("mensagens")
                 .child(idUsuarioRemetente)
                 .child(idUsuarioDestinatario);
 
+        recuperarMensagens();
+
+        //evento de clique na galeria
+        imageCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (i.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Bitmap imagem = null;
+            try {
+
+                switch (requestCode) {
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+
+                if (imagem != null) {
+
+                    //Recuperar dados de imagem para o firebase
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 90, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    /*Criar nome da imagem (classe UUID serve para criar identificadores únicos e que não se repetem,
+                    ela cria identificadores baseados na data, hora, minuto e segundo)*/
+                    String nomeImagem = UUID.randomUUID().toString();
+
+
+                    //configurar referencias do firebase
+                    final StorageReference imagemRef = storage.child("imagens")
+                            .child("fotos")
+                            .child(idUsuarioRemetente)
+                            .child(nomeImagem + ".jpeg");
+
+                    UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(ChatttActivity.this, "Erro ao fazer upload da imagem.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            imagemRef.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    String url = task.getResult().toString();
+
+                                    Mensagem mensagem = new Mensagem();
+                                    mensagem.setIdUsuario(idUsuarioRemetente);
+                                    //mensagem.setMensagem(nomeImagem);
+                                    mensagem.setImagem(url);
+
+                                    //salvar mensagem remetente
+                                    salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+
+                                    //salvar mensagem destinatario
+                                    salvarMensagem(idUsuarioDestinatario, idUsuarioRemetente, mensagem);
+
+                                    Toast.makeText(ChatttActivity.this, "Sucesso ao fazer upload da imagem.", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
     //em vez de dar reset à activity anterior, dá finish
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -169,16 +278,17 @@ public class ChatttActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     protected void onStart() {
         super.onStart();
-        recuperarMensagens();
+        //recuperarMensagens();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        mensagensRef.removeEventListener(childEventListenerMensagens);
+       // mensagensRef.removeEventListener(childEventListenerMensagens);
     }
 
     private void recuperarMensagens(){
