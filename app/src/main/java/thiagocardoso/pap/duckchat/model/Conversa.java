eecb228bef6b53a2ccc1db5ee194e0e9eff975d3
thiagoCardoso13 <thiagobafc@gmail.com>
2022-04-14 22:41:58 +0100

    Troca de mensagens funcionando e enviando imagens (otimizado) e fragment conversas funcionando - completo

---
+++ app/src/main/java/thiagocardoso/pap/duckchat/model/Conversa.java
@@ -0,0 +1,60 @@
+package thiagocardoso.pap.duckchat.model;
+
+import com.google.firebase.database.DatabaseReference;
+
+import thiagocardoso.pap.duckchat.config.ConfiguracaoFirebase;
+
+public class Conversa {
+
+    private String idRemetente;
+    private String idDestinatario;
+    private String ultimaMensagem;
+    private Usuario usuarioExibicao;
+
+    public Conversa() {
+    }
+
+    public void salvar(){
+
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference conversaRef = database.child("conversas");
+
+        conversaRef.child( this.getIdRemetente() )
+                .child( this.getIdDestinatario() )
+                .setValue( this );
+
+    }
+
+    public Usuario getUsuarioExibicao() {
+        return usuarioExibicao;
+    }
+
+    public void setUsuarioExibicao(Usuario usuarioExibicao) {
+        this.usuarioExibicao = usuarioExibicao;
+    }
+
+    public String getIdRemetente() {
+        return idRemetente;
+    }
+
+    public void setIdRemetente(String idRemetente) {
+        this.idRemetente = idRemetente;
+    }
+
+    public String getIdDestinatario() {
+        return idDestinatario;
+    }
+
+    public void setIdDestinatario(String idDestinatario) {
+        this.idDestinatario = idDestinatario;
+    }
+
+    public String getUltimaMensagem() {
+        return ultimaMensagem;
+    }
+
+    public void setUltimaMensagem(String ultimaMensagem) {
+        this.ultimaMensagem = ultimaMensagem;
+    }
+
+}
